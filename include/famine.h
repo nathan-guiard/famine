555256ac8856386b39d35ddede874f47a94313e8
nathan <nathan@Famine.myguest.virtualbox.org>
2024-10-25 17:24:02 +0200

    Starting print the whole file in an understandable format

--- include/famine.h
+++ include/famine.h
@@ -6,7 +6,7 @@
 /*   By: nguiard <nguiard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 03:01:42 by nguiard           #+#    #+#             */
-/*   Updated: 2024/09/10 03:03:59 by nguiard          ###   ########.fr       */
+/*   Updated: 2024/10/25 16:30:04 by nguiard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,19 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef	char * str;
+typedef	char *			str;
+typedef	unsigned char	byte;
 
+//	print_elf
+void	display_elf_header(Elf64_Ehdr elf_header);
+void	print_elf(FILE * const f);
+
+//	valid_file
 bool	valid_file(FILE **f, str filename);
+
+//	file_operations.c
+bool	insert(FILE *f, const void *data, const size_t size, size_t offset);
+
 
 #ifdef DEBUG
 # define printf(...) printf(__VA_ARGS__)
---
+++ src/print_elf.c
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_elf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nguiard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 15:44:08 by nguiard           #+#    #+#             */
+/*   Updated: 2024/10/25 17:20:48 by nguiard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "famine.h"
+
+static void	print_part(const byte *raw, size_t size, const char *label, const size_t end, bool quick);
+static void	print_line(const byte *raw, const long size, const char *label, const long label_size);
+static str	elf_header_label(Elf64_Ehdr header);
+
+#define LABEL_PER_LINE 42
+
+void	print_elf(FILE * const f) {
+	Elf64_Ehdr	elf_header;
+	Elf64_Phdr	prog_header;
+	size_t		current_offset = 0;
+	str			elf_label;
+
+	fread(&elf_header, sizeof(Elf64_Ehdr), 1, f); 
+	current_offset += sizeof(Elf64_Ehdr);
+
+	printf("\033[32m");
+	printf("+--------------------------------------------------+ 0x00000000\n");
+
+	elf_label = elf_header_label(elf_header);
+	print_part((const byte *)&elf_header, sizeof(Elf64_Ehdr), elf_label, current_offset, false);
+
+	fread(&prog_header, sizeof(Elf64_Phdr), 1, f); 
+	current_offset += sizeof(Elf64_Phdr);
+
+	printf("\033[33m");
+	print_part((const byte *)&prog_header, sizeof(Elf64_Phdr), "Program Header Table", current_offset, false);
+}
+
+static void	print_part(const byte *raw, size_t size, const char *label, const size_t end, bool quick) {
+	size_t	lines = size / 16 + (size % 16 ? 1 : 0);
+	size_t	label_size = strlen(label);
+
+	(void)quick;
+
+	for (size_t i = 0; i < lines; i++) {
+		print_line(raw + (i * 16), size - (i * 16),
+			 label ? label + (i * LABEL_PER_LINE) : NULL, label_size - (i * LABEL_PER_LINE));
+	}
+	printf("+--------------------------------------------------+ 0x%08x\n", (unsigned int)end);
+
+}
+
+static void	print_line(const byte *raw, const long size, const char *label, const long label_size) {
+
+	printf("| ");
+	for (int i = 0; i < 16; i++) {
+		if (i == 8)
+			printf(" ");
+		if (i > size) {
+			printf("   ");
+		} else {
+			printf("%02x ", raw[i]);
+		}
+	}
+	printf("| ");
+	if (label) {
+		for (int i = 0; i < LABEL_PER_LINE; i++) {
+			if (i <= label_size) {
+				printf("%c", label[i]);
+			}
+		}
+	}
+	printf("\n");
+}
+
+static str	elf_header_label(Elf64_Ehdr header) {
+	str	s;
+
+	s = malloc(42 * 4);
+	if (s == NULL)
+		return s;
+
+	sprintf(s, "Entry point: 0x%08x", (unsigned int)header.e_entry);
+
+	return s;
+}
+
+void display_elf_header(Elf64_Ehdr elf_header) {
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (int i = 0; i < EI_NIDENT; i++) {
+		printf("%02x ", elf_header.e_ident[i]);
+	}
+	printf("\n");
+
+	printf("  Class:                             ");
+	switch (elf_header.e_ident[EI_CLASS]) {
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("Invalid class\n");
+			break;
+	}
+
+	printf("  Data:                              ");
+	switch (elf_header.e_ident[EI_DATA]) {
+		case ELFDATA2LSB:
+			printf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian\n");
+			break;
+		default:
+			printf("Invalid data encoding\n");
+			break;
+	}
+
+	printf("  Version:                           %d\n", elf_header.e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %d\n", elf_header.e_ident[EI_OSABI]);
+	printf("  ABI Version:                       %d\n", elf_header.e_ident[EI_ABIVERSION]);
+	printf("  Type:                              0x%x\n", elf_header.e_type);
+	printf("  Machine:                           0x%x\n", elf_header.e_machine);
+	printf("  Version:                           0x%x\n", elf_header.e_version);
+	printf("  Entry point address:               0x%lx\n", elf_header.e_entry);
+	printf("  Start of program headers:          %ld (bytes into file)\n", elf_header.e_phoff);
+	printf("  Start of section headers:          %ld (bytes into file)\n", elf_header.e_shoff);
+	printf("  Flags:                             0x%x\n", elf_header.e_flags);
+	printf("  Size of this header:               %d (bytes)\n", elf_header.e_ehsize);
+	printf("  Size of program headers:           %d (bytes)\n", elf_header.e_phentsize);
+	printf("  Number of program headers:         %d\n", elf_header.e_phnum);
+	printf("  Size of section headers:           %d (bytes)\n", elf_header.e_shentsize);
+	printf("  Number of section headers:         %d\n", elf_header.e_shnum);
+	printf("  Section header string table index: %d\n", elf_header.e_shstrndx);
+}
